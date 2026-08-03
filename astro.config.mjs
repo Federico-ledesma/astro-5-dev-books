a1c0741a8328dffc83f3b60e1cb052a237cfbffa
NoxiusDev <federicoledesma542@gmail.com>
2026-08-02 23:28:13 -0300

    remove vercel generated files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig, envField } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server',
 
-  integrations: [tailwind()],
+  integrations: [
+    tailwind()
+  ],
 
   env: {
     schema: {
